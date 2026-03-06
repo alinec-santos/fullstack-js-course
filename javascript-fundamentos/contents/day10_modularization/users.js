c3bc505e8dceb762e2bdee9611432c97b7bc500d
alinec-santos <alinesantoscss17@gmail.com>
2026-03-05 21:22:17 -0300

    add day10_modularization

---
+++ javascript-fundamentos/contents/day10_modularization/users.js
@@ -0,0 +1,12 @@
+function addUser(name){
+    console.log("User add:",name);
+}
+
+function listUsers(){
+    console.log("Listing users");
+}
+
+module.exports = {
+    addUser,
+    listUsers
+};//aqui estamos importando as funções
